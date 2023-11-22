0a3789243f28ad11a37cc66803e3050c649a2f54
RuiZiHan <80934661+RuiZiHan@users.noreply.github.com>
2023-11-22 17:23:17 +0800

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -31,7 +31,7 @@ from repadapter import set_RepAdapter, set_RepWeight
 
 
 def visual(feat):
-    # t-SNE的最终结果的降维与可视化
+    # t-SNE
     ts = manifold.TSNE(n_components=2, init='pca', random_state=0)
 
     x_ts = ts.fit_transform(feat)
@@ -49,7 +49,7 @@ def plotlabels(S_lowDWeights, Trure_labels, method, model_struct, dataset, layer
     print("start plot t_SNE result")
     config = get_config(method=method, dataset_name=dataset)
     True_labels = Trure_labels.reshape((-1, 1))
-    S_data = np.hstack((S_lowDWeights, True_labels))  # 将降维后的特征与相应的标签拼接在一起
+    S_data = np.hstack((S_lowDWeights, True_labels))
     S_data = pd.DataFrame({'x': S_data[:, 0], 'y': S_data[:, 1], 'label': S_data[:, 2]})
 
     print(S_data)
@@ -58,14 +58,14 @@ def plotlabels(S_lowDWeights, Trure_labels, method, model_struct, dataset, layer
         f'/root/autodl-tmp/output_ours/visualization_output/t-SNE/{method}/{model_struct}/{dataset}_{layer_id}th_layer.csv')
     print(S_data.shape)  # [num, 3]
 
-    for index in range(config['class_num']):  # 类别数
+    for index in range(config['class_num']):  #class_num
         X = S_data.loc[S_data['label'] == index]['x']
         Y = S_data.loc[S_data['label'] == index]['y']
         # plt.scatter(X, Y, cmap='brg', s=100, marker=maker[index], c=colors[index], edgecolors=colors[index], alpha=0.65)
         plt.scatter(X, Y, cmap='brg', s=100, alpha=0.65)
 
-        plt.xticks([])  # 去掉横坐标值
-        plt.yticks([])  # 去掉纵坐标值
+        plt.xticks([])  
+        plt.yticks([])  
         plt.axis('off')
 
     # name = f'{method}_{model_struct}_{dataset}_{layer_id}blocks'
@@ -192,17 +192,17 @@ def analyse_t_SNE(method, model_struct, dataset, layer_id, best_score):
     # center = df.groupby('label').mean().to_numpy()
     X = df[['x', 'y']].to_numpy()
     label = df['label'].to_numpy()
-    # 轮廓系数 SC index
+    # SC index
     scoreSC = silhouette_score(X, label)
-    # 方差比 CH index
+    # CH index
     # scoreCH = calinski_harabasz_score(X, label)
-    # DB指数 DB index
+    # DB index
     # scoreDB = davies_bouldin_score(X, label)
 
     print(f"{layer_id}th layer")
-    print(f"轮廓系数[-1,1] 越大越好: {scoreSC}")
-    # print(f"方差比[0,+] 越大越好 by sklearn: {scoreCH}")
-    # print(f"DB指数[0,+] 越小越好 by sklearn: {scoreDB}")
+    print(f"SC Index[-1,1]: {scoreSC}")
+    # print(f"CH Index[0,+] by sklearn: {scoreCH}")
+    # print(f"DB Index[0,+] by sklearn: {scoreDB}")
     break_alalyse = False
     if model_struct == 'ViT':
         threshold = 0.3 * best_score
@@ -217,129 +217,6 @@ def analyse_t_SNE(method, model_struct, dataset, layer_id, best_score):
         break_alalyse = True
     return break_alalyse, scoreSC
 
-
-# total_center = center.mean(axis=0)  # 所有数据的中心点
-# count = np.array(df.groupby('label').count()).mean(axis=1)  # 各类的数量 label 0->cls_num-1
-#
-# class_wise_distance = 0
-# for label in range(config['class_num']):  # 0->cls_num-1
-#     class_wise_distance += np.linalg.norm(total_center - center[label])
-#
-# class_wise_distance /= config['class_num']
-# print("类间距离=", class_wise_distance)
-#
-# inner_distance = np.empty([config['class_num']])
-# point_data = np.array(df)
-# for i in point_data:
-#     label = int(i[-1])
-#     data = i[:-1]
-#     d = np.linalg.norm(data - center[label])  # 二范数
-#     inner_distance[label] += d
-#
-# print("类内距离=", (inner_distance / count).mean())
-
-
-# class imagenetdataset(data.Dataset):
-#     def __init__(self, transform=None):
-#         super().__init__()
-#         df = pd.read_csv('/root/autodl-tmp/datasets/imagenet/mini-imagenet-sxc/val.csv')
-#         # print(list(df['filename']))
-#         self.x = list(df['filename'])[0:1000]
-#         self.y = list(df['label'])[0:1000]
-#         self.transform = transform
-#
-#     def __len__(self):
-#         return len(self.x)
-#
-#     def __getitem__(self, item):
-#         img = self.transform(Image.open('/root/autodl-tmp/datasets/imagenet/mini-imagenet-sxc/images/' + self.x[item]))
-#         label = self.y[item]
-#         return img, label
-#
-#
-# transform = transforms.Compose([
-#     # transforms.Resize((224, 224), interpolation=3),
-#     transforms.Resize((224, 224), interpolation=InterpolationMode.BICUBIC),
-#     transforms.ToTensor(),
-#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-#
-# train_loader = torch.utils.data.DataLoader(
-#     imagenetdataset(transform=transform),
-#     batch_size=32, shuffle=True, drop_last=True,
-#     num_workers=4, pin_memory=True)
-
-
-def run(dataset):
-    train_dl, test_dl = get_data(dataset, evaluate=True, batch_size=40)
-
-    config = get_config('convpass', dataset)
-
-    model1 = create_model('vit_base_patch16_224_in21k', checkpoint_path='/root/autodl-tmp/pre_train/ViT-B_16.npz',
-                          drop_path_rate=0.1)
-
-    model2 = create_model('vit_base_patch16_224_in21k', checkpoint_path='/root/autodl-tmp/pre_train/ViT-B_16.npz',
-                          drop_path_rate=0.1)
-    # set_Convpass(model2, 'convpass', dim=8, s=config['scale'], xavier_init=config['xavier_init'])
-
-    model1.reset_classifier(config['class_num'])
-
-    model2.reset_classifier(config['class_num'])
-    # model2 = load(dataset, model2, 12)
-
-    model_list = []
-    for name, layer in model2.named_modules():
-        print(name)
-    for name, layer in model1.named_modules():
-        if 'mlp.drop' in name:
-            model_list.append(name)
-
-    # set_Convpass(model1, 'convpass', dim=8, s=config['scale'], xavier_init=config['xavier_init'])
-    # model1 = load(dataset, model1, 12)
-
-    cka = CKA(model1, model1,
-              model1_name="ViT-B",  # good idea to provide names to avoid confusion
-              model2_name="ViT-B",
-              model1_layers=model_list,  # List of layers to extract features from
-              model2_layers=model_list,  # extracts all layer features by default
-              device='cuda')
-
-    cka.compare(train_dl)  # secondary dataloader is optional
-    cka.plot_results(title=f'{dataset}_blocks')
-
-    results = cka.export()  # returns a dict that contains model names, layer names
-    # and the CKA matrix
-    result = results['CKA']  # 2D tensor
-    torch.save(result, f'/root/autodl-tmp/output_ours/visualization_output/CKA/{dataset}_blocks.pt')
-
-    # cka_tensor = rearrange(result, '(x y) (z w)-> x z (y w)', y=1, w=1)
-    #
-    # data = cka_tensor.numpy()
-    # data = data.mean(axis=-1, keepdims=False)
-    # fig, ax = plt.subplots()
-    # im = ax.imshow(data, origin='lower', cmap='magma')
-    # ax.set_xlabel("Layers ViT-B", fontsize=15)
-    # ax.set_ylabel("Layers ViT-B", fontsize=15)
-    # ax.set_title(f"{dataset}_blocks_mean", fontsize=18)
-    #
-    # add_colorbar(im)
-    # plt.tight_layout()
-    # plt.show()
-
-
-def load_run(dataset):
-    tensor = torch.load(f'/root/autodl-tmp/output_ours/visualization_output/CKA/{dataset}_blocks.pt')
-    data = tensor.numpy()
-
-    print("s(11,12)=", (data[10][11] + data[11][10]) / 2)
-    print("s(10,12)=", (data[9][11] + data[11][9]) / 2)
-    print("s(9,12)=", (data[8][11] + data[11][8]) / 2)
-    print("s(10,11)=", (data[10][9] + data[9][10]) / 2)
-    print("s(9,11)=", (data[10][8] + data[8][10]) / 2)
-    print("s(9,10)=", (data[9][8] + data[8][9]) / 2)
-    print("s(8,9)=", (data[7][8] + data[8][7]) / 2)
-    print("s(7,8)=", (data[7][6] + data[6][7]) / 2)
-
-
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--seed', type=int, default=42)
@@ -350,29 +227,10 @@ if __name__ == '__main__':
     print(args)
 
     set_seed(args.seed)
-    # run('dtd')
-
-    # load_run('cifar')
-
-    # # 设置散点形状
-    # maker = ['o', 's', '^', 's', 'p', '*', '<', '>', 'D', 'd', 'h', 'H']
-    # # 设置散点颜色
-    # colors = ['#e38c7a', '#656667', '#99a4bc', 'cyan', 'blue', 'lime', 'r', 'violet', 'm', 'peru', 'olivedrab',
-    #           'hotpink']
-    # # 图例名称
-    # Label_Com = ['a', 'b', 'c', 'd']
-    # # 设置字体格式
-    # font1 = {'family': 'Times New Roman',
-    #          'weight': 'bold',
-    #          'size': 32,
-    #          }
-
-    # run_t_SNE(method='convpass', model_struct='ViT', dataset='oxford_iiit_pet', layer_id=8)
-    # analyse_t_SNE(method='convpass', model_struct='ViT', dataset='oxford_iiit_pet', layer_id=8)
-
+    
     model_depth = 12 if args.model_struct == 'ViT' else 24
     best_score = 0
-    for i in range(model_depth):  # 0->11 0->4
+    for i in range(model_depth):
         run_t_SNE(method=args.method, model_struct=args.model_struct, dataset=args.dataset, layer_id=model_depth - i,
                   model_depth=model_depth)
         brear_or_not, score = analyse_t_SNE(method=args.method, model_struct=args.model_struct, dataset=args.dataset,
@@ -382,5 +240,3 @@ if __name__ == '__main__':
             print('best_score = ', best_score)
         if brear_or_not:
             break
-# for i in range(8, 13):
-#     analyse_t_SNE(method='convpass', model_struct='ViT', dataset='oxford_iiit_pet', layer_id=i)
